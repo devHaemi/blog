c14651901aefd5e9d33575330089fab717e4e9f1
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2025-05-09 14:57:39 +0900

    다크모드 토글 구현

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,33 @@
 import { Link } from 'react-router-dom';
+import { BsSun, BsMoonFill } from 'react-icons/bs';
+import { useContext } from 'react';
+
+import ThemeContext from 'context/ThemeContext';
 
 export default function Footer() {
+  // react v19 타입 호환 이슈 임시방편
+  const SunIcon = BsSun as any;
+  const MoonIcon = BsMoonFill as any;
+
+  const context = useContext(ThemeContext);
+
+  console.log(context);
+
   return (
     <footer>
       <Link to='/posts/new'>글쓰기</Link>
       <Link to='/posts'>게시글</Link>
       <Link to='/profile'>프로필</Link>
+      <>
+        {context.theme === 'light' ? (
+          <SunIcon onClick={context.toggleMode} className='footer__theme-btn' />
+        ) : (
+          <MoonIcon
+            onClick={context.toggleMode}
+            className='footer__theme-btn'
+          />
+        )}
+      </>
     </footer>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,14 +5,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from 'context/AuthContext';
+import { ThemeContextProvider } from 'context/ThemeContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <AuthContextProvider>
-    <Router>
-      <App />
-    </Router>
-  </AuthContextProvider>
+  <ThemeContextProvider>
+    <AuthContextProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthContextProvider>
+  </ThemeContextProvider>
 );
