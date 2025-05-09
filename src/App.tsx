ceb5defe3ca58f0e86cf1f32c8f488459433a51c
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2025-05-09 15:00:47 +0900

    다크모드 스타일 관련 클래스 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { ToastContainer } from 'react-toastify';
 
@@ -7,8 +7,10 @@ import 'react-toastify/dist/ReactToastify.css';
 import Router from './components/Router';
 import { app, db } from 'firebaseApp';
 import Loader from 'components/Loader';
+import ThemeContext from 'context/ThemeContext';
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
 
   const [init, setInit] = useState<boolean>(false);
@@ -28,10 +30,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === 'light' ? 'white' : 'dark'}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>
+    </div>
   );
 }
 
