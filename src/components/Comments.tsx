0a0139048982a6d56e6f2e1344f2456d38927487
전혜미@아이브랩 <hmjeon@aiblab.co.kr>
2025-05-12 11:10:56 +0900

    댓글 삭제하기

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,6 +1,6 @@
-import { PostProps } from 'components/PostList';
+import { CommentsInterface, PostProps } from 'components/PostList';
 import AuthContext from 'context/AuthContext';
-import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore';
 import { db } from 'firebaseApp';
 import { useContext, useState } from 'react';
 import { toast } from 'react-toastify';
@@ -63,6 +63,21 @@ export default function Comments({ post, getPost }: CommentProps) {
     }
   };
 
+  const handleDeleteComment = async (data: CommentsInterface) => {
+    const confirm = window.confirm('해당 댓글을 삭제하시겠습니까?');
+
+    if (confirm && post.id) {
+      const postRef = doc(db, 'posts', post.id);
+
+      await updateDoc(postRef, {
+        comments: arrayRemove(data),
+      });
+
+      toast.success('댓글이 삭제되었습니다.');
+      await getPost(post.id);
+    }
+  };
+
   return (
     <div className='comments'>
       <form className='comments__form' onSubmit={onSubmit}>
@@ -89,7 +104,14 @@ export default function Comments({ post, getPost }: CommentProps) {
               <div className='comment__profile-box'>
                 <div className='comment__email'>{comment.email}</div>
                 <div className='comment__date'>{comment.createdAt}</div>
-                <div className='comment__delete'>삭제</div>
+                {comment?.uid === user?.uid && (
+                  <div
+                    className='comment__delete'
+                    onClick={() => handleDeleteComment(comment)}
+                  >
+                    삭제
+                  </div>
+                )}
               </div>
               <div className='comment__text'>{comment.content}</div>
             </div>
